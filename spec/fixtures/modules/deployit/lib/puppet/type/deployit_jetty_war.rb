a7fbac2ae0b1e43110cafc5d1d97413f6d35a7a1
wian vos <wvos@xebia.com>
2013-05-10 12:55:30 +0200

    added some more type/provider combo's

---
+++ spec/fixtures/modules/deployit/lib/puppet/type/deployit_jetty_war.rb
@@ -0,0 +1,136 @@
+Puppet::Type.newtype(:deployit_jetty_war ) do
+
+      # this type is generated with genres.rb
+      # generated for deployit 3.8.5
+
+      desc 'adds a deployit_jetty_war ci to a remote deployit server'
+
+      ensurable do
+
+        desc 'deployit deployit_jetty_war resource state'
+
+        defaultto(:present)
+
+        newvalue(:present) do
+
+          provider.create
+
+        end
+
+        newvalue(:absent) do
+
+          provider.destroy
+
+        end
+
+      end
+
+      # name var
+      newparam(:id, :namevar => true) do
+        desc 'deployit_jetty_war id (full path required)'
+
+        validate do |value|
+              fail("invalid id") unless value =~ /^(Infrastructure|Environments)/
+         end
+      end
+
+      # general properties (default deployit stuff)
+
+      newparam(:deployit_username) do
+        desc 'a valid deployit user'
+        defaultto('admin')
+      end
+
+      newparam(:deployit_password) do
+        desc 'the password for the deployit user'
+        defaultto('admin')
+      end
+
+      newparam(:deployit_host) do
+        desc 'address of the deployit server'
+        defaultto('localhost')
+      end
+
+      newparam(:deployit_port) do
+        desc 'port to reach the deployit server on'
+        defaultto('4516')
+      end
+
+      newparam(:deployit_protocol) do
+        desc 'protocol to use in communication to the deployit host'
+        defaultto('http')
+      end
+    
+      newproperty(:startingweight) do
+         
+          desc 'Starting Weight'
+        
+        
+           
+        
+      end
+    
+      newproperty(:excludefilenamesregex) do
+         
+          desc 'Exclude File Names Regex'
+        
+        
+           
+        
+      end
+    
+      newproperty(:contextroot) do
+         
+          desc 'Context Root'
+        
+        
+           
+        
+      end
+    
+      newproperty(:tags , :array_matching => :all) do
+        def insync?(is)
+
+          # Comparison of Array's
+          # if either the should or the is (which we get from the providers envvars method is not a hash we'll fail
+          return false unless is.class == Array and should.class == Array
+
+          # now lets compare the two and see is a modify is needed
+          # haven't quite worked out yet what to do with extra values in the is hash
+          @should.each do |k|
+
+            # if is[k] is not equal to should[k] the insync? should return false
+            return false unless is.include?(k)
+
+          end
+          return false unless is.length == @should.length
+          true
+        end
+
+      end
+  
+      newproperty(:placeholders , :array_matching => :all) do
+        def insync?(is)
+
+          # Comparison of Array's
+          # if either the should or the is (which we get from the providers envvars method is not a hash we'll fail
+          return false unless is.class == Array and should.class == Array
+
+          # now lets compare the two and see is a modify is needed
+          # haven't quite worked out yet what to do with extra values in the is hash
+          @should.each do |k|
+
+            # if is[k] is not equal to should[k] the insync? should return false
+            return false unless is.include?(k)
+
+          end
+          return false unless is.length == @should.length
+          true
+        end
+
+      end
+  
+      
+      
+    end
+    
